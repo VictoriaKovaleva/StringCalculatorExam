9494724e2544943777f212bb7d492ca06d8238a5
vkovaleva <vkovaleva@voxelgroup.net>
2022-02-08 17:52:35 +0100

    feat: add condition for input is two

--- StringCalculator/StringCalculator/StringCalc.cs
+++ StringCalculator/StringCalculator/StringCalc.cs
@@ -6,6 +6,8 @@ namespace StringCalculator
         {
             if (input == "1")
                 return 1;
+            if (input == "2")
+                return 2;
             
             return 0;
         }
